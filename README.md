0b40a21f595de85d6ec05f223961a05c078e9952
Your Name <you@example.com>
2022-09-14 20:55:15 -0300

    Contatos Angular

---
+++ README.md
@@ -0,0 +1,10 @@
+# AngularContatos
+
+Esse projeto é desenvolvido com [Angular CLI](https://github.com/angular/angular-cli) version 14.2.2.
+
+## Configurações de desenvolvimento
+
+npm start ou ng serve -> para startar o projeto, endpoint padrão do Angular 
+localhost:4200
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContatosListagemComponent } from './contatos-listagem/contatos-listagem.component';
+import {ContatoService} from './contato.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatosListagemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [ContatoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
